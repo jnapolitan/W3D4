54590ce1b9a7ca0a17219dd2c64413a6c5aa0009
jnapolitan <julian.napolitan@gmail.com>
2018-11-08 17:01:31 -0800

    Create new rails project and polls app DB

---
+++ polls_app/app/models/response.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: responses
+#
+#  id               :bigint(8)        not null, primary key
+#  user_id          :integer          not null
+#  answer_choice_id :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+class Response < ApplicationRecord
+  
+  belongs_to :answer_choice,
+    primary_key: :id,
+    foreign_key: :answer_choice_id,
+    class_name: :AnswerChoice
+    
+  belongs_to :respondant,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+    
+  has_one :question,
+    through: :answer_choice,
+    source: :question
+    
+  def sibling_responses
+    self.question.responses.where.not(id: self.id)
+  end
+  
+  def respondant_already_answered?
+    
+  end
+    
+end
--- polls_app/db/seeds.rb
+++ polls_app/db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(username: "Albert")
+user2 = User.create(username: "Bob")
+user3 = User.create(username: "Carol")
+user4 = User.create(username: "Dave")
+user5 = User.create(username: "Elizabeth")
+
+poll1 = Poll.create(title: "Albert's poll", author_id: user1.id)
+poll2 = Poll.create(title: "Bob's poll", author_id: user2.id)
+
+q1 = Question.create(text: "Do you like me?", poll_id: poll1.id)
+q2 = Question.create(text: "Will you vote for me?", poll_id: poll1.id)
+q3 = Question.create(text: "What is love?", poll_id: poll1.id)
+
+q4 = Question.create(text: "Do you think rent is too damn high?", poll_id: poll2.id)
+q5 = Question.create(text: "Will you vote for me?", poll_id: poll2.id)
+q6 = Question.create(text: "What is love?", poll_id: poll2.id)
+
+a1 = AnswerChoice.create(text: "Yes", question_id: q1.id)
+a2 = AnswerChoice.create(text: "No", question_id: q1.id)
+a3 = AnswerChoice.create(text: "Yes", question_id: q2.id)
+a4 = AnswerChoice.create(text: "No", question_id: q2.id)
+a5 = AnswerChoice.create(text: "Baby don't hurt me...", question_id: q3.id)
+a6 = AnswerChoice.create(text: "Don't hurt me no more.", question_id: q3.id)
+
+a7 = AnswerChoice.create(text: "Yes", question_id: q4.id)
+a8 = AnswerChoice.create(text: "No", question_id: q4.id)
+a9 = AnswerChoice.create(text: "Yes", question_id: q5.id)
+a10 = AnswerChoice.create(text: "No", question_id: q5.id)
+a11 = AnswerChoice.create(text: "Baby don't hurt me...", question_id: q6.id)
+a12 = AnswerChoice.create(text: "Don't hurt me no more.", question_id: q6.id)
+
+Response.create(user_id: user4.id, answer_choice_id: a7.id)
+Response.create(user_id: user4.id, answer_choice_id: a10.id)
+Response.create(user_id: user4.id, answer_choice_id: a12.id)
+
+Response.create(user_id: user3.id, answer_choice_id: a2.id)
+Response.create(user_id: user3.id, answer_choice_id: a3.id)
+Response.create(user_id: user3.id, answer_choice_id: a5.id)
+
+Response.create(user_id: user5.id, answer_choice_id: a2.id)
+Response.create(user_id: user5.id, answer_choice_id: a3.id)
+Response.create(user_id: user5.id, answer_choice_id: a5.id)
+
+
+
+
+
